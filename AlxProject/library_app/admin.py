f088895e794f6e2b42d315074c5cc0ee5ac3c3c7
Valencia Masika <masikavalencia62@gmail.com>
2025-08-31 14:30:59 +0300

    Library Management System

--- AlxProject/library_app/admin.py
+++ AlxProject/library_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Book, Member
+from .models import Book, Member, Borrow
 
 admin.site.register(Book)
 admin.site.register(Member)
+admin.site.register(Borrow)
--- AlxProject/library_app/models.py
+++ AlxProject/library_app/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
     published_date = models.DateField()
     isbn = models.CharField(max_length=13, unique=True)
 
@@ -11,10 +12,22 @@ class Book(models.Model):
 
 
 class Member(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     joined_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return self.name
+
+
+class Borrow(models.Model):
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrow_date = models.DateTimeField(default=timezone.now)
+    return_date = models.DateTimeField(null=True, blank=True)
+
+    def is_returned(self):
+        return self.return_date is not None
+
+    def __str__(self):
+        return f"{self.member.name} borrowed {self.book.title}"
